ce6d4aff73d1b6ceacded9a17b01b7109acc1073
nataliiashylina@gmail.com <nataliiashylina@gmail.com>
2024-06-12 14:51:19 +0200

    Individual project, added api tests and methods

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -2,6 +2,7 @@ import requests
 
 
 class GitHub:
+    BASE_URL = "https://api.github.com"
 
     def get_user_defunkt(self):
         r = requests.get('https://api.github.com/users/defunkt')
@@ -16,15 +17,35 @@ class GitHub:
         return body
     
     def get_user(self, username):
-        r = requests.get(f'https://api.github.com/users/{username}')
+        r = requests.get(f"{self.BASE_URL}/users/{username}")
         body = r.json()
 
         return body
     
     def search_repo(self, name):
-        r = requests.get("https://api.github.com/search/repositories",
-            params={"q": name}                 
-        )
+        r = requests.get(f"{self.BASE_URL}/search/repositories", params={"q": name})
         body = r.json()
 
         return body
+    
+    def get_emojis(self):
+        r = requests.get(f"{self.BASE_URL}/emojis")
+        body = r.json()
+
+        return body
+    
+    def list_commits(self, owner, repo):
+        r = requests.get(f"{self.BASE_URL}/repos/{owner}/{repo}/commits")
+        body = r.json()
+        
+        return body
+    
+    def get_repo_languages(self, owner, repo):
+        r = requests.get(f"{self.BASE_URL}/repos/{owner}/{repo}/languages")
+
+        return r.json()
+
+    def get_commit(self, owner, repo, sha):
+        r = requests.get(f"{self.BASE_URL}/repos/{owner}/{repo}/commits/{sha}")
+
+        return r.json()
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -42,3 +42,28 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo('s')
     assert r['total_count'] != 0    
+
+@pytest.mark.api
+def test_get_emojis(github_api):
+    emojis = github_api.get_emojis()
+    assert 'astronaut' in emojis
+    assert emojis['astronaut'] == "https://github.githubassets.com/images/icons/emoji/unicode/1f9d1-1f680.png?v8"
+
+@pytest.mark.api
+def test_list_commits(github_api):
+    commits = github_api.list_commits('NataliaShylina', 'Prometheus_HW')
+    assert len(commits) > 0
+    assert 'commit' in commits[0]
+    assert 'sha' in commits[0]    
+
+@pytest.mark.api
+def test_get_repo_languages(github_api):
+    languages = github_api.get_repo_languages('NataliaShylina', 'Prometheus_HW')
+    assert 'Python' in languages or 'Java' in languages
+
+@pytest.mark.api
+def test_get_commit(github_api):
+    commit = github_api.get_commit('NataliaShylina', 'Prometheus_HW', 'd96c4e1acbc8750987a157282d72855e53ceb06f')
+    assert 'sha' in commit
+    assert 'commit' in commit
+    assert commit['sha'] == 'd96c4e1acbc8750987a157282d72855e53ceb06f'    
